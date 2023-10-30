7dc12901eb6256dab638c043efa74933416b362a
MahinMuhammad <amimahinurrahman@gmail.com>
2023-10-30 22:39:38 +0600

    Initial Commit

---
+++ Al Quran Stream/Al_Quran_StreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Al_Quran_StreamApp.swift
+//  Al Quran Stream
+//
+//  Created by Md. Mahinur Rahman on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Al_Quran_StreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
